d4d60eca7e579a6144ad2f6a6c098295202d8434
cl <cl_fighting@163.com>
2017-04-13 15:03:27 +0800

    add hint to delete

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -12,9 +12,19 @@ class JobsController < ApplicationController
 
   def create
     @job = Job.new(job_params)
-    @job.save
-    redirect_to jobs_path
+    if @job.save
+       redirect_to jobs_path
+     else
+       render :new
+     end
   end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path, alert: "Job deleted!"
+  end
+
 
   private
 
