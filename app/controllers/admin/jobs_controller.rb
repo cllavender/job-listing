aa52dc2b660796dc5737ec59d451f61b6674aa8d
Lin Chen <cl_fighting@163.com>
2017-04-20 03:14:17 +0000

    adjust structure of the file

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -6,11 +6,58 @@ class Admin::JobsController < ApplicationController
       @jobs = Job.all.order("created_at DESC")
     end
 
+
+  
+   def show
+    @job = Job.find(params[:id])
+   end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+
+    if @job.save
+       redirect_to admin_jobs_path
+     else
+       render :new
+    end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to admin_jobs_path, notice: "Update success!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to admin_jobs_path, alert: "Job deleted!"
+  end
+
   def require_is_admin
     unless current_user.admin?
       flash[:alert] = 'You are not admin'
       redirect_to root_path
     end
   end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
+  end
+
+
 
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -10,46 +10,7 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
-  def new
-    @job = Job.new
-  end
-
-  def create
-    @job = Job.new(job_params)
-    @job.user = current_user
-
-    if @job.save
-       redirect_to admin_jobs_path
-     else
-       render :new
-     end
-  end
-
-  def edit
-    @job = Job.find(params[:id])
-  end
-
-  def update
-    @job = Job.find(params[:id])
-    if @job.update(job_params)
-      redirect_to admin_jobs_path, notice: "Update success!"
-    else
-      render :new
-    end
-  end
-
-  def destroy
-    @job = Job.find(params[:id])
-    @job.destroy
-    redirect_to admin_jobs_path, alert: "Job deleted!"
-  end
-
-
-  private
-
-  def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
-  end
+ 
 
 
 end
